5e6dc835ca808f767bf4fcd1a49bacde4b888114
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-04-03 18:53:30 +0200

    Use 7zip to build output archive

--- build-libmp3lame.cmd
+++ build-libmp3lame.cmd
@@ -3,7 +3,7 @@ set PATH=%PATH%;..\
 
 if /I "%TARGET_PLATFORM%" == "amd64" (
     sed -i -e s/machine:I386/machine:x64/ Makefile.MSVC
-    nmake -f Makefile.MSVC MSVCVER=Win64 libmp3lame.dll
+    nmake -f Makefile.MSVC MSVCVER=Win64 ASM=YES libmp3lame.dll
 ) else (
     nmake -f Makefile.MSVC libmp3lame.dll
 )
