f9b6b099ec1b978903c7f7ad4a18945a8a068a84
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-04-03 18:14:04 +0200

    Add MSVCVER=Win64 on x64 platform

--- build-libmp3lame.cmd
+++ build-libmp3lame.cmd
@@ -1,5 +1,9 @@
 @echo off
 call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %TARGET_PLATFORM%
 set PATH=%PATH%;..\
-nmake -f Makefile.MSVC libmp3lame.dll
 
+if "%TARGET_PLATFORM%"=="x64" (
+    nmake -f Makefile.MSVC MSVCVER=Win64 libmp3lame.dll
+) else (
+    nmake -f Makefile.MSVC libmp3lame.dll
+)
