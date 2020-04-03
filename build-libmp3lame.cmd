878b6f74daddf1250755a9dd50b27e70bbbcca7b
Andrzej Ciarkowski <andrzej.ciarkowski@gmail.com>
2020-04-03 18:32:24 +0200

    WIP

--- build-libmp3lame.cmd
+++ build-libmp3lame.cmd
@@ -2,6 +2,7 @@ call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" %TAR
 set PATH=%PATH%;..\
 
 if /I "%TARGET_PLATFORM%" == "amd64" (
+    sed -i -e s/machine:I386/machine:x64/ Makefile.MSVC
     nmake -f Makefile.MSVC MSVCVER=Win64 libmp3lame.dll
 ) else (
     nmake -f Makefile.MSVC libmp3lame.dll
